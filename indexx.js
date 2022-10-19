0d54c28bda5dd543ad26f9a078de6b46259be7f7
Glydene Gudes <111970096+GlydeneGudes@users.noreply.github.com>
2022-10-19 09:06:48 -0500

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,8 @@
+//JavaScript function that accepts an array as parameter and  
+//returns the elements of an array of numbers that are less than 18.
+
+
+let array = [30, 15, 12, 21, 19]
+
+const result = array.filter(arr => arr < 18)
+console.log(result)
